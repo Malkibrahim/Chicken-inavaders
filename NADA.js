54727068392b5722aa8371ae58355d1481f532d2
NadaYoussef-d <59627123+NadaYoussef-d@users.noreply.github.com>
2020-01-12 22:52:04 +0200

    Delete NADA.js

--- NADA.js
+++
@@ -1,8 +0,0 @@
-console.log('helloooooo')
-function ReapetImage (howManyTimes){
-    var picture = document.getElementById('pic')
-    for (var i = 0; i < howManyTimes; i++) {
-        picture.innerHTML += "<img src='chicken.png' alt='chicken' width='60px' height='55px'>"
-    }
-}
-ReapetImage(30);
