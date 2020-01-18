aa35a580891eb1afa1610730ea2f979833678fca
Malkibrahim <37645796+Malkibrahim@users.noreply.github.com>
2020-01-18 23:04:44 +0200

    level up

--- js/game.mjs
+++ js/game.mjs
@@ -54,7 +54,7 @@ function introButtons(){
 
         generalState.destroyEnemys=[];
        console.log(this==document.getElementById('newPlayer'));}
-        else if(this== document.getElementById('contBtn')){
+        else {
          generalState.destroyEnemys=localStorage.getItem("deadEnemy")
          $('#score').text(parseInt(localStorage.getItem('score')));
          $('#lives').text(parseInt(localStorage.getItem('lives'))); 
@@ -97,40 +97,43 @@ function introButtons(){
       return min + Math.random() * (max - min);
     }
     
-    function createShip($container) {
+    function createShip(container) {
       generalState.shipX = gameWidth / 2;
       generalState.shipY = gameHieght - 50;
-      const $ship = document.createElement("img");
-      $ship.src = "img/player.png";
-      $ship.className = "ship";
-      $ship.width = shapeWidth;
-      $container.appendChild($ship);
-      setPosition($ship, generalState.shipX, generalState.shipY);
+      const ship = document.createElement("img");
+      ship.src = "img/player.png";
+      ship.className = "ship";
+      ship.width = shapeWidth;
+      container.appendChild(ship);
+      setPosition(ship, generalState.shipX, generalState.shipY);
     }
     
-    function destroyShip($container, ship) {
-      $container.removeChild(ship);
+    function destroyShip(container, ship) {
+      container.removeChild(ship);
       generalState.gameOver = true;
       generalState.lives--;
       lives.text(generalState.lives);
       if(generalState.lives<parseInt(localStorage.getItem('lives'))){
         localStorage.setItem('lives',generalState.lives);
       }
+      if(lives==0){
+        gameOver();
+      }
       console.log(generalState.lives);
       const audio = new Audio("sound/sfx-lose.ogg");
       audio.play();
     }
     
-    function createLaser($container, x, y) {
-      const $element = document.createElement("img");
-      $element.src = "img/fire.png";
-      $element.className = "laser";
-      $container.appendChild($element);
-      const laser = { x, y, $element };
+    function createLaser(container, x, y) {
+      const element = document.createElement("img");
+      element.src = "img/fire.png";
+      element.className = "laser";
+      container.appendChild(element);
+      const laser = { x, y, element };
       generalState.lasers.push(laser);
       const audio = new Audio("sound/sfx-laser1.ogg");
       audio.play();
-      setPosition($element, x, y);
+      setPosition(element, x, y);
       // debugger;
       generalState.laserLimit--;
       rockets.text(generalState.laserLimit);
@@ -140,7 +143,7 @@ function introButtons(){
       console.log(generalState.laserLimit);
     }
     
-    function updateship(dt, $container) {
+    function updateship(dt,container) {
       if (generalState.leftPressed) {
         generalState.shipX -= dt * shapeSpeed;
       }  
@@ -155,7 +158,7 @@ function introButtons(){
         );  
         
         if (generalState.spacePressed && generalState.shipCooldown <= 0) {
-          createLaser($container, generalState.shipX, generalState.shipY);
+          createLaser(container, generalState.shipX, generalState.shipY);
           generalState.shipCooldown = laserSlowing;
         }  
         if (generalState.shipCooldown > 0) {
@@ -166,25 +169,25 @@ function introButtons(){
         setPosition(ship, generalState.shipX, generalState.shipY);
       }  
       
-      function updateLasers(dt, $container) {
+      function updateLasers(dt, container) {
         const lasers = generalState.lasers;
         for (let i = 0; i < lasers.length; i++) {
           const laser = lasers[i];
           laser.y -= dt * laserSpeed;
           if (laser.y < 0) {
-            destroyLaser($container, laser);
+            destroyLaser(container, laser);
           }
-          setPosition(laser.$element, laser.x, laser.y);
-          const r1 = laser.$element.getBoundingClientRect();
+          setPosition(laser.element, laser.x, laser.y);
+          const r1 = laser.element.getBoundingClientRect();
           const chickens = generalState.chickens;
           for (let j = 0; j < chickens.length; j++) {
             const enemy = chickens[j];
             if (enemy.isDead) continue;
-            const r2 = enemy.$element.getBoundingClientRect();
+            const r2 = enemy.element.getBoundingClientRect();
             if (intersection(r1, r2)) {
               // Enemy was hit
-              destroyEnemy($container, enemy);
-              destroyLaser($container, laser);
+              destroyEnemy(container, enemy);
+              destroyLaser(container, laser);
               break;
             }
           }
@@ -192,8 +195,8 @@ function introButtons(){
         generalState.lasers = generalState.lasers.filter(e => !e.isDead);
       }
       
-      function destroyLaser($container, laser) {
-       $container.removeChild(laser.$element);
+      function destroyLaser(container, laser) {
+       container.removeChild(laser.element);
       //  generalState.destroyEnemys.push(desroyed.cssText)
       //  console.log(destroyed.style.cssText);
 
@@ -201,26 +204,26 @@ function introButtons(){
         laser.isDead = true;
       }
       
-      function createChicken($container, x, y) {
-        const $element = document.createElement("img");
-        $element.src = "img/chicken.png";
-        $element.className = "enemy";
-        $container.appendChild($element);
+      function createChicken(container, x, y) {
+        const element = document.createElement("img");
+        element.src = "img/chicken.png";
+        element.className = "enemy";
+        container.appendChild(element);
         const chicken = {
           x,
           y,
           cooldown: rand(0.5, chickenSlowDown),
-          $element
+          element
         };
         generalState.chickens.push(chicken);
         // setPosition($element, x, y);
         if (!generalState.destroyEnemys.includes(`translate(${x}px, ${y}px)`)){
 
-          $element.style.transform = `translate(${x}px, ${y}px)`;
+          element.style.transform = `translate(${x}px, ${y}px)`;
         }
       }
       
-      function updateChicken(dt, $container) {
+      function updateChicken(dt, container) {
         const dx = Math.sin(generalState.lastTime / 1000.0) * 50;
         const dy = Math.cos(generalState.lastTime / 1000.0) * 10;
         
@@ -229,54 +232,61 @@ function introButtons(){
           const enemy = chickens[i];
           const x = enemy.x + dx;
           const y = enemy.y + dy;
-          setPosition(enemy.$element, x, y);
+          setPosition(enemy.element, x, y);
           enemy.cooldown -= dt;
           if (enemy.cooldown <= 0) {
-            createChickenLaser($container, x, y);
+            createChickenLaser(container, x, y);
             enemy.cooldown = chickenSlowDown;
           }
         }
         generalState.chickens = generalState.chickens.filter(e => !e.isDead);
+        if(generalState.chickens.length==0){
+          levelUp();
+        }
       }
       
-      function destroyEnemy($container, enemy) {
-      var desroyed=  $container.removeChild(enemy.$element);
+      function destroyEnemy(container, enemy) {
+      var desroyed=  container.removeChild(enemy.element);
         // generalState.destroyEnemys.push(desroyed.style.cssText);
         // console.log(generalState.destroyEnemys);
        generalState.score+=5;
        score.text(generalState.score);
-      //  if(generalState.score>parseInt(localStorage.getItem('score'))){
+       if(generalState.score>parseInt(localStorage.getItem('score'))){
         localStorage.setItem('score',generalState.score);
-      // }
+       }
+       if(generalState.score%20==0){
+        //  levelUp();
+         generalState.laserLimit+=5;
+       }
        console.log(generalState.score);
         enemy.isDead = true;
       }
       
-      function createChickenLaser($container, x, y) {
-        const $element = document.createElement("img");
-        $element.src = "img/egg3.png";
-        $element.className = "enemy-laser";
-        $container.appendChild($element);
-        const laser = { x, y, $element };
+      function createChickenLaser(container, x, y) {
+        var element = document.createElement("img");
+        element.src = "img/egg3.png";
+        element.className = "enemy-laser";
+        container.appendChild(element);
+        var laser = { x, y, element };
         generalState.chickenLasers.push(laser);
-        setPosition($element, x, y);
+        setPosition(element, x, y);
       }
       
-      function updateEnemyLasers(dt, $container) {
+      function updateEnemyLasers(dt, container) {
         const lasers = generalState.chickenLasers;
         for (let i = 0; i < lasers.length; i++) {
-          const laser = lasers[i];
+          var laser = lasers[i];
           laser.y += dt * laserSpeed;
           if (laser.y > gameHieght) {
-            destroyLaser($container, laser);
+            destroyLaser(container, laser);
           }
-          setPosition(laser.$element, laser.x, laser.y);
-          const r1 = laser.$element.getBoundingClientRect();
-          const ship = document.querySelector(".ship");
-          const r2 = ship.getBoundingClientRect();
+          setPosition(laser.element, laser.x, laser.y);
+          var r1 = laser.element.getBoundingClientRect();
+          var ship = document.querySelector(".ship");
+          var r2 = ship.getBoundingClientRect();
           if (intersection(r1, r2)) {
             // ship was hit
-            destroyShip($container, ship);
+            destroyShip(container, ship);
             break;
           }
         }
@@ -285,15 +295,15 @@ function introButtons(){
       
       
       function init() {
-        const $container = document.querySelector(".game");
-        createShip($container);
+        const container = document.querySelector(".game");
+        createShip(container);
         
         const enemySpacing = (gameWidth - chickenHorizntalPadding * 2) / (chickenPerRow - 1);
         for (let j = 0; j < 3; j++) {
           const y = chickenVirticalPadding + j * chickenSpace;
           for (let i = 0; i < chickenPerRow; i++) {
             const x = i * enemySpacing + chickenHorizntalPadding;
-            createChicken($container, x, y);
+            createChicken(container, x, y);
           }
         }
       }
@@ -307,21 +317,21 @@ function introButtons(){
         const currentTime = Date.now();
         const dt = (currentTime - generalState.lastTime) / 1000.0;
 
-        if (generalState.gameOver) {
-          document.querySelector(".game-over").style.display = "block";
-          return;
-        }
+        // if (generalState.gameOver) {
+        //   document.querySelector(".game-over").style.display = "block";
+        //   return;
+        // }
         
         if (shipHasWon()) {
           document.querySelector(".congratulations").style.display = "block";
           return;
         }
         
-        const $container = document.querySelector(".game");
-        updateship(dt, $container);
-        updateLasers(dt, $container);
-        updateChicken(dt, $container);
-        updateEnemyLasers(dt, $container);
+        var container = document.querySelector(".game");
+        updateship(dt, container);
+        updateLasers(dt, container);
+        updateChicken(dt, container);
+        updateEnemyLasers(dt, container);
         
         generalState.lastTime = currentTime;
         
@@ -356,3 +366,20 @@ function introButtons(){
       // console.log(score.text());
 ``
     }
+function levelUp(){
+  $('.upgrade').removeClass('hide');
+  $('.upgrade').fadeOut(1000);
+  if($('.upgrade').fadeOut()){
+    
+    introButtons();
+  }
+
+}
+function gameOver(){
+  $('.upgrade').children[0].text('Game Over');
+  $('.upgrade').fadeIn(1000);
+  $('.upgrade').fadeOut(1000);
+  $('.whole').addClass('hide');
+  $('.intro').removeClass('hide');
+
+}
